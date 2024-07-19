e3db66cfd18a5b645046b5952cc031cfd0b7db77
Matthew Reidsma <reidsmam@gvsu.edu>
2024-07-19 19:09:02 +0000

    Update libhours.js

--- src/js/libhours.js
+++ src/js/libhours.js
@@ -95,7 +95,7 @@ jQuery(document).ready(function($){
 			} else {
 				if (libname in libData) {
 					var library = libData[libname];
-<i class="fa-regular fa-clock"></i><span>Today&nbsp;' + library.jsonData.hours + '</span>;
+text= '<i class="fa-regular fa-clock"></i><span>Today&nbsp;' + library.jsonData.hours + '</span>';
 				} else {
 					text = "ERROR: Unknown library name '"+libname+"'.";
 					if (typeof console != "undefined") {
